0a20cadaec5cfda5a12a400e8c6617cf8903f009
Ildar Meyker <ildar.meyker@gmail.com>
2023-07-21 22:16:03 +0300

    Fix.

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,7 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/js/modules/developers.js":
-/*!**************************************!*\
-  !*** ./src/js/modules/developers.js ***!
-  \**************************************/
+/***/ 968:
 /***/ (function() {
 
 var iconStyles = " font-size:50px;";
@@ -25,10 +22,7 @@ if (pubdate < new Date()) {
 
 /***/ }),
 
-/***/ "./src/js/modules/masks.js":
-/*!*********************************!*\
-  !*** ./src/js/modules/masks.js ***!
-  \*********************************/
+/***/ 488:
 /***/ (function() {
 
 $(function () {
@@ -41,10 +35,7 @@ $(function () {
 
 /***/ }),
 
-/***/ "./src/js/modules/tabs.js":
-/*!********************************!*\
-  !*** ./src/js/modules/tabs.js ***!
-  \********************************/
+/***/ 241:
 /***/ (function() {
 
 var setActiveButton = function setActiveButton($tabs, index) {
@@ -129,31 +120,16 @@ $(function () {
 /******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
 /******/ 	}();
 /******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	}();
-/******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be in strict mode.
 !function() {
 "use strict";
-/*!************************!*\
-  !*** ./src/js/main.js ***!
-  \************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modules_developers__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/developers */ "./src/js/modules/developers.js");
+/* harmony import */ var _modules_developers__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(968);
 /* harmony import */ var _modules_developers__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_modules_developers__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _modules_tabs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/tabs */ "./src/js/modules/tabs.js");
+/* harmony import */ var _modules_tabs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(241);
 /* harmony import */ var _modules_tabs__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_modules_tabs__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _modules_masks__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/masks */ "./src/js/modules/masks.js");
+/* harmony import */ var _modules_masks__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(488);
 /* harmony import */ var _modules_masks__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_modules_masks__WEBPACK_IMPORTED_MODULE_2__);
 
 
